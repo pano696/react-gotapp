3c950f087a2ec42d7217977b78492be7f67ef8d1
Мурзагалиев Павел Серикович <murzagaliev@obalt.baltimore.ru>
2019-06-24 13:55:51 +0300

    Lesson 9 Hard

--- src/services/GotService.js
+++ src/services/GotService.js
@@ -2,6 +2,7 @@ export default class gotService {
 
   constructor() {
     this._apiBase = 'https://www.anapioficeandfire.com/api/';
+    this._noData = 'no data :(';
   }
 
   async getResource(url) {
@@ -39,33 +40,37 @@ export default class gotService {
   };
 
   _transformCharacter(char) {
+    const noData = 'no data :('
     return {
-        url: char.url,
-        name: char.name,
-        gender: char.gender,
-        born: char.born,
-        died: char.died,
-        culture: char.culture
+        url: char.url ? char.url : noData,
+        name: char.name ? char.name : noData,
+        gender: char.gender ? char.gender : noData,
+        born: char.born ? char.born : noData,
+        died: char.died ? char.died : noData,
+        culture: char.culture ? char.culture : noData
     }
   }
 
   _transformHoses(house) {
+    const noData = 'no data :('
     return {
-      name: house.name,
-      region: house.region,
-      words: house.words,
-      titles: house.titles,
-      overlord: house.overlord,
-      ancentralWeapons: house.ancentralWeapons
+      url: house.url ? house.url : noData,
+      name: house.name ? house.name : noData,
+      region: house.region ? house.region : noData,
+      words: house.words ? house.words : noData,
+      titles: house.titles ? house.titles : noData,
+      overlord: house.overlord ? house.overlord : noData,
+      ancentralWeapons: house.ancentralWeapons ? house.ancentralWeapons : noData
     }
   }
 
   _transformBook(book) {
+    const noData = 'no data :('
     return {
-      name: book.name,
-      numberOfPages: book.numberOfPages,
-      publiser: book.publiser,
-      released: book.released
+      name: book.name ? book.name : noData,
+      numberOfPages: book.numberOfPages ? book.numberOfPages : noData,
+      publiser: book.publiser ? book.publiser : noData,
+      released: book.released ? book.released : noData
     }
   }
 }
